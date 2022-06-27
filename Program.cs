b681436f0bd2e755657ed4ab8b6612ec329c6c78
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-06-27 15:11:10 +0200

    fourth snack done

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
 ﻿/*
+ 10- Fai inserire un numero, che chiamaremo N, all'utente. Genera N arry, ognunoi formato da 10 numeri casuali tra 1 e 100.
+
  5- Il software chiede all'utente di inserire il numero. Se il numero inserito è pari, stampa il numero, se è dispari stampa il numero successivo
  4- Calcola la somma e la media dei numeri da 2 a 10;
 
@@ -14,6 +16,30 @@ namespace csharp_snacks_1
     {
         static void Main(string[] args)
         {
+            //snack 10
+            Random rnd = new Random();
+            int num = rnd.Next();
+            Console.Write("Inserire un numero: ");
+            int arrays = Int32.Parse(Console.ReadLine());
+
+            for (int i = 0; i < arrays; i++)
+            {
+                int[] randomNumberArray = new int[10];
+                    for (int k = 0; k < 10; k++)
+                    {
+                        randomNumberArray[k] = rnd.Next(1,100);
+                        Console.Write(randomNumberArray[k]);
+                        if(k < 10-1)
+                        {
+                            Console.Write(",");
+                        }
+                    }
+                    Console.Write("\n");
+
+            }
+
+
+            return;
             //snack 5
             Console.Write("Inserire un numero: ");
             int newUserNumber = Int32.Parse(Console.ReadLine());
@@ -27,8 +53,8 @@ namespace csharp_snacks_1
                 Console.WriteLine(newUserNumber);  
             }
 
-
-            return;
+            
+            
             //snack 4
             int[] numbers = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             int numbersSum = 0;
