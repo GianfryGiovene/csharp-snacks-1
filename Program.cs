0740624286009bc42a9fb1b41c9cee247214b9a8
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-06-27 14:13:13 +0200

    first snack done

--- Program.cs
+++ Program.cs
@@ -1,10 +1,26 @@
-﻿namespace csharp_snacks_1
+﻿/*
+ Il software deve chiedere per 10 volte all’utente di inserire un numero.
+Il programma stampa la somma di tutti i numeri inseriti.
+
+ 
+*/
+
+namespace csharp_snacks_1
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            int sum = 0;
+
+            for(int i = 0; i < 10; i++)
+            {
+                Console.Write("Inserire il numero: ");
+                int userNumber = Int32.Parse(Console.ReadLine());
+                sum += userNumber; 
+            }
+
+            Console.WriteLine(sum);
         }
     }
 }
