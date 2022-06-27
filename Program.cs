170ddc1ea92f28432525390c5e29cf7b121c4899
Giovanni_Francesco_Giovene <gianfri.gvn@gmail.com>
2022-06-27 14:55:27 +0200

    third snack done

--- Program.cs
+++ Program.cs
@@ -1,8 +1,9 @@
 ﻿/*
+ 5- Il software chiede all'utente di inserire il numero. Se il numero inserito è pari, stampa il numero, se è dispari stampa il numero successivo
  4- Calcola la somma e la media dei numeri da 2 a 10;
 
- 3-Il software deve chiedere per 10 volte all’utente di inserire un numero.
-Il programma stampa la somma di tutti i numeri inseriti.
+ 3- Il software deve chiedere per 10 volte all’utente di inserire un numero.
+    Il programma stampa la somma di tutti i numeri inseriti.
 
  
 */
@@ -13,6 +14,21 @@ namespace csharp_snacks_1
     {
         static void Main(string[] args)
         {
+            //snack 5
+            Console.Write("Inserire un numero: ");
+            int newUserNumber = Int32.Parse(Console.ReadLine());
+
+            if (newUserNumber % 2 != 0)
+            {
+                Console.WriteLine(newUserNumber +1 );
+            }
+            else
+            {
+                Console.WriteLine(newUserNumber);  
+            }
+
+
+            return;
             //snack 4
             int[] numbers = { 2, 3, 4, 5, 6, 7, 8, 9, 10 };
             int numbersSum = 0;
@@ -26,7 +42,9 @@ namespace csharp_snacks_1
             average = numbersSum / numbers.Length;
 
             Console.WriteLine($"La somma dei numeri è: {numbersSum}. La media dei numeri è: {average}");
-            return;
+            
+
+
             //snack 3
             int sum = 0;
 
